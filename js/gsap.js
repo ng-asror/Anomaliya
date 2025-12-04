1763e28b040d2c1777b66e6b3ecd673b02d825df
Асрор Шодиев <144030637+ng-asror@users.noreply.github.com>
2025-12-04 23:29:36 +0500

    add: results section

--- js/gsap.js
+++ js/gsap.js
@@ -25,6 +25,7 @@ ScrollTrigger.create({
 /*===== hero section =====*/
 const heroes = gsap.utils.toArray('.heroes__img');
 const distance = heroes[0].offsetHeight;
+const heroBtn = document.querySelector('.hero-wrapper__btn');
 
 gsap.to(heroes, {
 	y: `-=${distance * (heroes.length / 3)}`,
@@ -40,7 +41,7 @@ const encounter__card = encounter.querySelector('.encounter__card');
 
 ScrollTrigger.create({
 	trigger: encounter,
-	start: 'center bottom',
+	start: 'top center',
 	onEnter: () => encounterAboutFN(),
 })
 
@@ -58,3 +59,61 @@ function encounterAboutFN() {
 		ease: 'power1.out'
 	})
 }
+
+
+/*===== results section =====*/
+ScrollTrigger.create({
+	trigger: '.card-learning',
+	start: 'top center',
+	onEnter: () => {
+		const tl = gsap.timeline();
+		tl.to('#count', {
+			textContent: 37,
+			duration: 3,
+			snap: { textContent: 1 },
+			ease: "power1.out"
+		}, 0)
+
+			.to('img.ellipsis', {
+				rotate: 150,
+				duration: 3,
+				ease: 'power1.out'
+			}, 0);
+	}
+});
+
+
+ScrollTrigger.create({
+	trigger: '.card-partners',
+	start: 'top center',
+	onEnter: () => {
+		const images = [...gsap.utils.toArray('.partners .partner-img'), '.main-partner img']
+		gsap.to(images, {
+			scale: 1.2,
+			translateY: "-8",
+			duration: 3,
+			ease: 'power3.out'
+		})
+	}
+})
+ScrollTrigger.create({
+	trigger: '.card-overcome',
+	start: 'top center',
+	onEnter: () => {
+		gsap.to('.card-overcome img', {
+			translate: "-15px -15px",
+			duration: 3,
+			ease: 'power3.out'
+		})
+	}
+})
+const wrapper = document.querySelector("#results");
+const sticky = document.querySelector(".results-wrapper__left");
+ScrollTrigger.create({
+	trigger: wrapper,
+	start: "top+=60 top",
+	end: `${wrapper.offsetHeight += sticky.offsetHeight}px`,
+	pin: sticky,
+	pinSpacing: false,
+	scrub: true
+});
